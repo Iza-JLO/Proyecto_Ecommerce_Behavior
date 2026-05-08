d81505d427bff79f457220dacc77c04185b6e01d
Jorge Loza Olvera <jorge.loza5670@alumnos.udg.mx>
2026-05-08 10:46:29 -0600

    df.info para nuevos datos seleccionados

--- src/model_1.py
+++ src/model_1.py
@@ -1,7 +1,7 @@
 from eda_selection import target_encoder_data
 import pandas as pd
 
-#NOTA: FALTA TRANSFORMAR LA COLUMNA ORDER_STATUS o BORRARLA
+#NOTA: FALTA TRANSFORMAR LA COLUMNA ORDER_STATUS/DAT OF WEEK/PAYMENT_METHOD o (USAR ONEHOT ENCODED)
 
 df_model = pd.read_csv("data/processed/df_visualizacion.csv")
 print("-"*50)
