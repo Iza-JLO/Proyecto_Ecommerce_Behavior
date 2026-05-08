a35baa2e1a57856824c324cc58d667c17c987b04
Jorge Loza Olvera <jorge.loza5670@alumnos.udg.mx>
2026-05-08 09:58:02 -0600

    WIP: model_1.py agregado; particion de conjuntos de prueba y entrenamiento

---
+++ src/model_1.py
@@ -0,0 +1,12 @@
+from eda_selection import target_encoder_data
+import pandas as pd
+
+df_model = pd.read_csv("data/processed/df_visualizacion.csv")
+print("-"*50)
+print("DataFrame cargado con éxito!")
+
+X_train, X_test, y_train, y_test = target_encoder_data(df=df_model)
+print("-"*50)
+print("Conjuntos de entrenamiento y pruba cargados con éxito!")
+
+print(f"Longitud de conjutos:\nX_train: {len(X_train)}\nX_test: {len(X_test)}\ny_train: {len(y_train)}\ny_test: {len(y_test)}")
