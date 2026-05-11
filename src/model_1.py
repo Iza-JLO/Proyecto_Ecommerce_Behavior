d88ebb8bfb028c129f70d6d9f3990824661a1b07
Jorge Loza Olvera <jorge.loza5670@alumnos.udg.mx>
2026-05-11 09:35:38 -0600

    Cambio de llamadas de función encoded_data

--- src/model_1.py
+++ src/model_1.py
@@ -1,4 +1,4 @@
-from eda_selection import target_encoder_data
+from eda_selection import encoded_data
 import pandas as pd
 
 #NOTA: FALTA TRANSFORMAR LA COLUMNA ORDER_STATUS/DAT OF WEEK/PAYMENT_METHOD o (USAR ONEHOT ENCODED)
@@ -9,8 +9,9 @@ print("DataFrame cargado con éxito!")
 print(f"{df_model.head()}")
 print(df_model.info())
 
-X_train, X_test, y_train, y_test = target_encoder_data(df=df_model)
+X_train, X_test, y_train, y_test = encoded_data(df=df_model)
 print("-"*50)
 print("Conjuntos de entrenamiento y pruba cargados con éxito!")
 
 print(f"Longitud de conjutos:\nX_train: {len(X_train)}\nX_test: {len(X_test)}\ny_train: {len(y_train)}\ny_test: {len(y_test)}")
+print(f"X_train: \n{X_train.info()}")
